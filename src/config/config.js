bdebd5083ad41591a1d824e3d24471dc27ee47ff
root <maresp85@gmail.com>
2024-04-11 19:24:06 -0500

    Improve statistics in report

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 process.env.PORT = process.env.PORT || 3000;
 
 // Vencimiento del token
-process.env.CAD_TOKEN = "48h";
+process.env.CAD_TOKEN = "24h";
 
 //firma del token
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo-ang';
